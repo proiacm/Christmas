6f7a7e87293a7877c2c0592daf590741dc66c925
proiacm <proiacm@gmail.com>
2020-11-12 14:00:27 -0700

    intergrate neal-react countdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Countdown from './Countdown/Countdown';
 
 class App extends Component {
 
   render() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
     return (
       <div className="App">
-          Christmas Countdown 2020
+        <h3 className="title">Christmas is coming in:</h3>
+        <Countdown date={`${year}-12-25T00:00:00`} />
       </div>
     );
   }
