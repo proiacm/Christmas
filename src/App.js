5c04b189de7d574d34cb590a55da2d70ec38365e
proiacm <proiacm@gmail.com>
2020-12-06 23:02:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Countdown from './Countdown/Countdown';
 import Snowfall from 'react-snowfall';
-// import LottieAnimation from './Lottie';
-// import home from './Animation/xmas.json';
 import Footer from './Footer/Footer';
-import Lights from './Lights/Lights';
+// import Lights from './Lights/Lights';
 
 class App extends Component {
 
@@ -15,7 +13,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Snowfall color='#FFFFFF'/>
-        <Lights />
+        {/* <Lights /> */}
         <div className="text">
           <Countdown date={`${year}-12-25T00:00:00`} />
           until Christmas!</div>
